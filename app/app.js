50420e62e2ceb713e156a25c2f59025b32c37ed0
Richard Shea <rshea@thecubagroup.com>
2016-03-16 23:41:11 +1300

    Start of the enumerables demo

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import common from './common';
+//import common from './common';
+import common from './mixins/common';
 import secondMixin from './secondMixin';
 
 let App;
@@ -23,6 +24,8 @@ const obj = Ember.Object.extend(common,{
 });
 const object = obj.create();
 
+console.log("Simple Mixin Example***********************");
+console.log("");
 console.log(object.get('objprop')); // outputs 'This is an Ember object property'
 console.log(object.get('property1')); // outputs 'This is a mixin property'
 console.log(object.get('isEditing')); // output 'false'
@@ -34,7 +37,52 @@ console.log("");
 const secondobj = Ember.Object.extend(common, secondMixin, {
     objprop: 'This is an Ember object Property'
 });
+console.log("Multi Mixin Example***********************");
+console.log("");
 const secondObject = secondobj.create();
 console.log(secondObject.get('secondProperty'));
 
+console.log("*******************************************");
+console.log("");
+
+console.log("Enumerable Example - forEach ************");
+console.log("");
+const students = ['Erik', 'Jim', 'Shelly', 'Kate'];
+students.forEach(function(item, index) {
+    console.log(`Student #${index+1}: ${item}`);
+});
+console.log("Enumerable Example - map ****************");
+console.log("");
+
+//Use `map` to convert every item to uppercase
+//Notice that the array `students` is hardcoded
+//into the resulting function assigned to 
+//`upperCaseStudent`
+const upperCaseStudent = students.map(function(item) {
+    return item.toUpperCase();
+});
+upperCaseStudent.forEach(function(item, index) {
+    console.log(`student #${index+1}: ${item}`);
+});
+
+//Use `mapBy` to deal with arrays of objects
+const student = Ember.Object.extend({
+    name: 'Erik Hanchett',
+    age: 18
+});
+const teacher = Ember.Object.extend({
+    name: 'John P. Smith',
+    age: 48
+});
+const t= teacher.create();
+const s = student.create();
+const people = [s, t];
+
+//Output the `name` property of each 
+//of the objects contained in the 
+//`people` array
+console.log(people.mapBy('name'));
+
+console.log("*******************************************");
+console.log("");
 export default App;
--- app/mixins/common.js
+++ app/mixins/common.js
@@ -1,4 +1,13 @@
+/*global Ember */
+/*exported common */
+/*exported Ember */
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+    property1: 'This is a mixin property',
+    edit: function(){
+        console.log('Starting to edit');
+        this.set('isEditing', true);
+    },
+    isEditing: false
 });
