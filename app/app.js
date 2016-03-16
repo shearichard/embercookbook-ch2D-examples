d2a924232d37a07d0280c25b5ef5e744281abf1c
Richard Shea <rshea@thecubagroup.com>
2016-03-16 22:54:27 +1300

    Initial mixin demo complete

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import common from './common';
 
 let App;
 
@@ -14,5 +15,22 @@ App = Ember.Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const obj = Ember.Object.extend(common,{
+    objprop: 'This is an Ember object property'
+});
+const object = obj.create();
+
+
+console.log(object.get('objprop')); // outputs 'This is an Ember object property'
+console.log(object.get('property1')); // outputs 'This is a mixin property'
+console.log(object.get('isEditing')); // output 'false'
+object.edit(); //Starting to edit
+console.log(object.get('isEditing')); //outputs 'true'
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 export default App;
